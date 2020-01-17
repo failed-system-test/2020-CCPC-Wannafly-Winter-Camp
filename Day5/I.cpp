34ec8f2643e05051ba9f561dc3fc0035e65d2d3d
Shengliang Cai <cubercsl@163.com>
2020-01-17 09:07:47 +0800

    upsolve Day5

--- Day5/I.cpp
+++ Day5/I.cpp
@@ -1,4 +1,3 @@
-// MLE or WA
 #include <bits/stdc++.h>
 #ifndef ONLINE_JUDGE
 #include <local_debug.hpp>
@@ -6,103 +5,69 @@
 #define debug(...)
 #endif
 
-const int MAXN = 1 << 11;
-
 using i64 = long long;
-
+const int MAXN = 1 << 12;
+i64 tree[MAXN][MAXN];
 int n, m1, m2;
 i64 a[MAXN][MAXN];
 
-struct inNode {
-    std::shared_ptr<inNode> lson, rson;
-    i64 value;
-    inNode() : value(0), lson(nullptr), rson(nullptr) {}
-    void maintain() { value = std::max(lson->value, rson->value); }
-};
+inline int id(int l, int r) { return l + r | l != r; }
+#define o id(l, r)
+#define lson id(l, m)
+#define rson id(m + 1, r)
 
-struct outNode {
-    std::shared_ptr<outNode> lson, rson;
-    std::shared_ptr<inNode> value;
-    outNode() : value(nullptr), lson(nullptr), rson(nullptr) {}
-    void maintain() { merge(value, lson->value, rson->value, 1, n); }
-    static void merge(std::shared_ptr<inNode>& o, std::shared_ptr<inNode>& ls,
-                      std::shared_ptr<inNode> rs, int l, int r) {
-        debug("merge", l, r);
-        if (o == nullptr) {
-            o = std::make_shared<inNode>();
-        }
-        assert(o);
-        assert(ls);
-        assert(rs);
-        if (l == r) {
-            o->value = std::max(ls->value, rs->value);
-            debug(o->value, ls->value, rs->value);
-            return;
-        }
-        const int m = l + r >> 1;
-        merge(o->lson, ls->lson, rs->lson, l, m);
-        merge(o->rson, ls->rson, rs->rson, m + 1, r);
-        o->maintain();
-    }
-};
-
-void build(std::shared_ptr<inNode>& o, int x, int l, int r) {
-    if (o == nullptr) {
-        o = std::make_shared<inNode>();
-    }
-    debug("y", l, r);
+void merge(i64* tree, i64* ls, i64* rs, int l, int r) {
     if (l == r) {
-        o->value = a[x][l];
-        debug(o->value);
+        tree[o] = std::max(ls[o], rs[o]);
         return;
     }
     const int m = l + r >> 1;
-    build(o->lson, x, l, m);
-    build(o->rson, x, m + 1, r);
-    o->maintain();
+    merge(tree, ls, rs, l, m);
+    merge(tree, ls, rs, m + 1, r);
+    tree[o] = std::max(tree[lson], tree[rson]);
 }
 
-void build(std::shared_ptr<outNode>& o, int l, int r) {
-    if (o == nullptr) {
-        o = std::make_shared<outNode>();
-    }
-    debug("x", l, r);
+void build(int x, i64* tree, int l, int r) {
     if (l == r) {
-        build(o->value, l, 1, n);
+        tree[o] = a[x][l];
         return;
     }
     const int m = l + r >> 1;
-    build(o->lson, l, m);
-    build(o->rson, m + 1, r);
-    o->maintain();
+    build(x, tree, l, m);
+    build(x, tree, m + 1, r);
+    tree[o] = std::max(tree[lson], tree[rson]);
 }
 
-i64 query(std::shared_ptr<inNode> o, int l, int r, int y1, int y2) {
-    assert(o);
-    debug(l, r, y1, y2);
+void build(int l, int r) {
+    if (l == r) {
+        build(l, tree[o], 1, n);
+        return;
+    }
+    const int m = l + r >> 1;
+    build(l, m);
+    build(m + 1, r);
+    merge(tree[o], tree[lson], tree[rson], 1, n);
+}
+
+i64 query(i64* tree, int l, int r, int y1, int y2) {
     if (y1 <= l && r <= y2) {
-        debug(o->value);
-        return o->value;
+        return tree[o];
     }
     const int m = l + r >> 1;
     i64 ans = 0;
-    if (y1 <= m) ans = std::max(ans, query(o->lson, l, m, y1, y2));
-    if (m < y2) ans = std::max(ans, query(o->rson, m + 1, r, y1, y2));
+    if (y1 <= m) ans = std::max(ans, query(tree, l, m, y1, y2));
+    if (m < y2) ans = std::max(ans, query(tree, m + 1, r, y1, y2));
     return ans;
 }
 
-i64 query(std::shared_ptr<outNode> o, int l, int r, int x1, int x2, int y1,
-          int y2) {
-    assert(o);
-    debug(l, r, x1, x2, y1, y2);
+i64 query(int l, int r, int x1, int x2, int y1, int y2) {
     if (x1 <= l && r <= x2) {
-        return query(o->value, 1, n, y1, y2);
+        return query(tree[o], 1, n, y1, y2);
     }
     const int m = l + r >> 1;
     i64 ans = 0;
-    if (x1 <= m) ans = std::max(ans, query(o->lson, l, m, x1, x2, y1, y2));
-    if (m < x2) ans = std::max(ans, query(o->rson, m + 1, r, x1, x2, y1, y2));
-    debug(ans);
+    if (x1 <= m) ans = std::max(ans, query(l, m, x1, x2, y1, y2));
+    if (m < x2) ans = std::max(ans, query(m + 1, r, x1, x2, y1, y2));
     return ans;
 }
 
@@ -125,10 +90,9 @@ int main() {
             a[i][j] += a[i][j - 1];
         }
     }
-    std::shared_ptr<outNode> rt = nullptr;
-    build(rt, 1, n);
+    build(1, n);
     for (int i = 0, x1, y1, x2, y2; i < m2; i++) {
         scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
-        printf("%lld\n", query(rt, 1, n, x1, x2, y1, y2));
+        printf("%lld\n", query(1, n, x1, x2, y1, y2));
     }
 }
